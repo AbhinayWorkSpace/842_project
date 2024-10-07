de85a72e67733d8d8bd8d299332b2f6f6ac86d62
Matt DiRisio <dirisiom@msu.edu>
2024-10-07 19:52:09 -0400

    started feature engineering

--- fraud_load.py
+++ fraud_load.py
@@ -1,11 +1,50 @@
 import pandas as pd
 import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from textblob import TextBlob
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.stem import WordNetLemmatizer
+from gensim import corpora
+from gensim.models import LdaMulticore
+import string
+import re
 
-def load_data():
-    data = pd.read_csv('fraud.csv')
-    data['label'] = data['label'].map({'OR': 0, 'CG': 1})
+def feature_engineer(df):
+    dl_path = './nltk_data'
+    nltk.download('punkt', download_dir=dl_path)
+    nltk.download('wordnet', download_dir=dl_path)
+    nltk.download('stopwords', download_dir=dl_path)
+    nltk.download('averaged_perceptron_tagger', download_dir=dl_path)
+    nltk.data.path.append(dl_path)
+
+    # load data and format some columns
+    df['label'] = df['label'].map({'OR': 0, 'CG': 1})
     # change label header to 'fraud'
-    data = data.rename(columns={'label': 'fraud', 'text_': 'text'})
-    return data
+    df = df.rename(columns={'label': 'fraud', 'text_': 'text'})
 
-load_data()
+
+    # feature engineering
+
+    # XLNet embeddings will be valuable, but that is done in training and not here
+
+
+    # one hot encoding of category
+    # I'm not totally certain if we'll want this but i thought it can't hurt for now
+    # if we dont want it we can just drop the column
+    df = pd.get_dummies(df, columns=['category'], prefix='category')
+    # df.drop(columns=['category'], inplace=True)
+
+    df['normalized_rating'] = df['rating'] / 5.0
+
+    # text features
+    df['text_length'] = df['text'].apply(len)
+    df['word_count'] = df['text'].apply(lambda x: len(x.split()))
+    df['sentence_count'] = df['text'].apply(lambda x: len(sent_tokenize(x)))
+    return df
+
+def load_df():
+    df = pd.read_csv('fraud.csv')
+    df = feature_engineer(df)
+    return df
